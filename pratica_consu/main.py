0e97e6de2172cb353a008097a64f6e744b72ec85
lourranny <santana.lourranny@escolar.ifrn.edu.br>
2025-06-23 16:01:27 -0300

    2_bimestre

---
+++ pratica_consu/main.py
@@ -0,0 +1,23 @@
+import requests
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:8000"
+    r = requests.get(f"{url}/livros")
+    print(r.text)
+    livro = {
+        "titulo": "python",
+        "ano": 2024,
+        "edicao": 1
+    }
+    r = requests.post (f'{url}/livros', json=livro)
+    print(r.status_code)
+    print(r.text)
+
+    pesquisa = "python"
+  
+    r = requests.get (f'{url}/livros/{pesquisa}')
+    print(r.status_code)
+    print(r.text)
+
+    r = requests.delete (f'{url}/livros/{pesquisa}')
+    print(r.status_code)
